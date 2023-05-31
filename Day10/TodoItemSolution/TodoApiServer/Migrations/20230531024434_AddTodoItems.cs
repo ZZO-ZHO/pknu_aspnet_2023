14572d00e3be4b77babdfff57114974450f0beae
jeong jaeyeong <xstar202@naver.com>
2023-05-31 17:23:58 +0900

    5월 31일

---
+++ Day10/TodoItemSolution/TodoApiServer/Migrations/20230531024434_AddTodoItems.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoApiServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTodoItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
